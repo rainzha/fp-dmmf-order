5dbacb57be89ed7a9b27e27a0528e141afad892b
rainzha <rainzha@163.com>
2026-08-08 14:29:01 +0800

    增加断言库assertj‑core:3.27.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
 group = "org.rainzha"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(26)
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -13,6 +19,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
