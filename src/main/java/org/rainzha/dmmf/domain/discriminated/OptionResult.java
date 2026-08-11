5d54b3b293ebd3cec55ff028bd8106299dae9ba7
rainzha <rainzha@163.com>
2026-08-11 14:33:13 +0800

    改进OptionResult

--- src/main/java/org/rainzha/dmmf/domain/discriminated/OptionResult.java
+++ src/main/java/org/rainzha/dmmf/domain/discriminated/OptionResult.java
@@ -1,35 +1,32 @@
 package org.rainzha.dmmf.domain.discriminated;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * F# option 通用容器 Some<T> / None
  * 用于表达“有值/无值”，对应 AcknowledgeOrder 返回 OrderAcknowledgmentSent option
  */
 public sealed interface OptionResult<T> permits OptionResult.Some, OptionResult.None {
+    // 修改：R 统一作为匹配返回类型，两个分支都支持返回 R
+    <R> R match(Function<T, R> someCase, Supplier<R> noneCase);
 
-    <R> R match(Function<T, R> someCase, Runnable noneCase);
-
-    // 存在值分支
     record Some<T>(T value) implements OptionResult<T> {
         @Override
-        public <R> R match(Function<T, R> someCase, Runnable noneCase) {
+        public <R> R match(Function<T, R> someCase, Supplier<R> noneCase) {
             return someCase.apply(value);
         }
     }
 
-    // 空值单例
     OptionResult<?> NONE = new None<>();
 
     record None<T>() implements OptionResult<T> {
         @Override
-        public <R> R match(Function<T, R> someCase, Runnable noneCase) {
-            noneCase.run();
-            return null;
+        public <R> R match(Function<T, R> someCase, Supplier<R> noneCase) {
+            return noneCase.get();
         }
     }
 
-    // 静态构造快捷方法
     static <T> OptionResult<T> some(T value) {
         return new Some<>(value);
     }
